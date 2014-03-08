2ca3db9d5cacc14a66ed7e64b4a332267e8a8b50
Robin Green <greenrd@greenrd.org>
2014-03-08 07:37:47 +0000

    Now it's an actual major mode

--- task-support.el
+++ task-support.el
@@ -1,4 +1,4 @@
 (defun task-add (new-task)
   (interactive "MTask to add: ")
-  (call-process "task" nil t nil "add" new-task)
-)
+  (call-process "task" nil nil nil "add" new-task)
+  (taskwarrior))
--- taskwarrior-mode.el
+++ taskwarrior-mode.el
@@ -1,7 +1,41 @@
+(require 'term)
+(defvar taskwarrior-mode-map
+  (let ((map (make-sparse-keymap)))
+    (set-keymap-parent map special-mode-map)
+    (define-key map "g" 'taskwarrior)
+    (define-key map "a" 'task-add)
+    map)
+  "Keymap for `taskwarrior-mode'.")
+
+(define-derived-mode taskwarrior-mode term-mode "Taskwarrior"
+  "Mode for interacting with Taskwarrior")
+
+(defvar taskwarrior-buffer-name "*Taskwarrior*"
+  "Name of buffer where output of Taskwarrior is put.")
+
+(defun taskwarrior-sentinel (proc msg)
+  "Sentinel for taskwarrior buffers."
+  (let ((buffer (process-buffer proc)))
+    (when (memq (process-status proc) '(signal exit))
+      (if (null (buffer-name buffer))
+	  ;; buffer killed
+	  (set-process-buffer proc nil)
+	(delete-process proc)))))
+
 (defun taskwarrior ()
   (interactive)
-  (kill-buffer "*Taskwarrior*")
-  (set-buffer (get-buffer-create "*Taskwarrior*"))
-  (term-mode)
-  (term-exec (current-buffer) "Taskwarrior" "task" nil (list "unblocked"))
-  (switch-to-buffer "*Taskwarrior*"))
+  (set-buffer (get-buffer-create taskwarrior-buffer-name))
+  (erase-buffer)
+  (taskwarrior-mode)
+
+  ; Based on code from term.el in emacs
+  (let ((proc (get-buffer-process (current-buffer)))) ; Blast any old process.
+	(when proc (delete-process proc)))
+  ;; Crank up a new process
+  (let ((proc (term-exec-1 "Taskwarrior" (current-buffer) "task" (list "unblocked"))))
+    (make-local-variable 'term-ptyp)
+    (setq term-ptyp t)
+    (set-marker (process-mark proc) (point))
+    (set-process-filter proc 'term-emulate-terminal)
+    (set-process-sentinel proc 'taskwarrior-sentinel)
+    (switch-to-buffer (current-buffer))))
